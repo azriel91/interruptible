6036e2b2f72c1cc8439e747c26417e2b681a0b49
Azriel Hoh <azriel91@gmail.com>
2023-08-02 07:17:28 +1200

    Add tests in `InterruptibleFutureExt`.

--- src/interruptible_future_ext.rs
+++ src/interruptible_future_ext.rs
@@ -98,3 +98,57 @@ where
         InterruptibleFutureResult::new(self, interrupt_rx)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::ops::ControlFlow;
+
+    use futures::FutureExt;
+    use tokio::{join, sync::oneshot};
+
+    use super::InterruptibleFutureExt;
+    use crate::InterruptSignal;
+
+    #[tokio::test]
+    async fn interrupt_overrides_control_future_return_value() {
+        let (interrupt_tx, interrupt_rx) = oneshot::channel::<InterruptSignal>();
+        let (ready_tx, ready_rx) = oneshot::channel::<()>();
+
+        let interruptible_control = async {
+            let () = ready_rx.await.expect("Expected to be notified to start.");
+            ControlFlow::Continue(())
+        }
+        .boxed()
+        .interruptible_control(interrupt_rx);
+
+        let interrupter = async move {
+            interrupt_tx
+                .send(InterruptSignal)
+                .expect("Expected to send `InterruptSignal`.");
+            ready_tx
+                .send(())
+                .expect("Expected to notify sleep to start.");
+        };
+
+        let (control_flow, ()) = join!(interruptible_control, interrupter);
+
+        assert_eq!(ControlFlow::Break(InterruptSignal), control_flow);
+    }
+
+    #[tokio::test]
+    async fn interrupt_after_control_future_completes_does_not_override_value() {
+        let (interrupt_tx, interrupt_rx) = oneshot::channel::<InterruptSignal>();
+
+        let interruptible_control = async { ControlFlow::<InterruptSignal, ()>::Continue(()) }
+            .boxed()
+            .interruptible_control(interrupt_rx);
+
+        let interrupter = async move {
+            let (Ok(()) | Err(InterruptSignal)) = interrupt_tx.send(InterruptSignal);
+        };
+
+        let (control_flow, ()) = join!(interruptible_control, interrupter);
+
+        assert_eq!(ControlFlow::Continue(()), control_flow);
+    }
+}

2a9c5326a143f4256a023a812fe6fdac36578b7e
Azriel Hoh <azriel91@gmail.com>
2023-10-11 19:38:18 +1300

    Change `InterruptibleStream` to manage interruptions itself.

--- src/interruptible_stream.rs
+++ src/interruptible_stream.rs
@@ -1,38 +1,55 @@
-use std::pin::Pin;
+use std::{ops::ControlFlow, pin::Pin};
 
 use futures::{
     stream::Stream,
     task::{Context, Poll},
 };
-use stream_cancel::Valved;
+use tokio::sync::mpsc::{self, error::TryRecvError};
+
+use crate::{InterruptSignal, OwnedOrMutRef};
 
 #[derive(Debug)]
-pub struct InterruptibleStream<S> {
+pub struct InterruptibleStream<'rx, S> {
     /// Underlying stream that produces values.
-    stream_valved: Valved<S>,
+    stream: Pin<Box<S>>,
+    /// Receiver for interrupt signal.
+    interrupt_rx: OwnedOrMutRef<'rx, mpsc::Receiver<InterruptSignal>>,
 }
 
-impl<S> InterruptibleStream<S>
+impl<'rx, S> InterruptibleStream<'rx, S>
 where
     S: Stream,
 {
     /// Returns a new `InterruptibleStream`, wrapping the provided stream.
-    pub(crate) fn new(stream_valved: Valved<S>) -> InterruptibleStream<S> {
-        Self { stream_valved }
+    pub(crate) fn new(
+        stream: S,
+        interrupt_rx: OwnedOrMutRef<'rx, mpsc::Receiver<InterruptSignal>>,
+    ) -> InterruptibleStream<S> {
+        Self {
+            stream: Box::pin(stream),
+            interrupt_rx,
+        }
     }
 }
 
-impl<S> Stream for InterruptibleStream<S>
+impl<'rx, S> Stream for InterruptibleStream<'rx, S>
 where
-    S: Stream + std::marker::Unpin,
+    S: Stream,
 {
-    type Item = S::Item;
+    type Item = ControlFlow<(InterruptSignal, S::Item), S::Item>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        Pin::new(&mut self.stream_valved).poll_next(cx)
+        self.stream.as_mut().poll_next(cx).map(|item_opt| {
+            item_opt.map(|item| match self.interrupt_rx.try_recv() {
+                Ok(InterruptSignal) => ControlFlow::Break((InterruptSignal, item)),
+                Err(TryRecvError::Empty | TryRecvError::Disconnected) => {
+                    ControlFlow::Continue(item)
+                }
+            })
+        })
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        self.stream_valved.size_hint()
+        self.stream.size_hint()
     }
 }
--- src/interruptible_stream_ext.rs
+++ src/interruptible_stream_ext.rs
@@ -1,21 +1,29 @@
 use futures::stream::Stream;
-use stream_cancel::Valved;
-use tokio::sync::oneshot;
+use tokio::sync::mpsc;
+
+#[cfg(feature = "ctrl_c")]
+use tokio::sync::mpsc::error::SendError;
 
 use crate::{InterruptSignal, InterruptibleStream};
 
 /// Provides the `.interruptible()` method for `Stream`s to stop producing
 /// values when an interrupt signal is received.
 pub trait InterruptibleStreamExt {
+    /// Overrides this `Stream`'s poll value when an interrupt signal is
+    /// received.
+    ///
+    /// # Parameters
+    ///
+    /// * `interrupt_rx`: Channel receiver of the interrupt signal.
     fn interruptible(
         self,
-        interrupt_rx: oneshot::Receiver<InterruptSignal>,
-    ) -> InterruptibleStream<Self>
+        interrupt_rx: &mut mpsc::Receiver<InterruptSignal>,
+    ) -> InterruptibleStream<'_, Self>
     where
         Self: Sized;
 
     #[cfg(feature = "ctrl_c")]
-    fn interruptible_ctrl_c(self) -> InterruptibleStream<Self>
+    fn interruptible_ctrl_c(self) -> InterruptibleStream<'static, Self>
     where
         Self: Sized;
 }
@@ -26,35 +34,93 @@ where
 {
     fn interruptible(
         self,
-        interrupt_rx: oneshot::Receiver<InterruptSignal>,
+        interrupt_rx: &mut mpsc::Receiver<InterruptSignal>,
     ) -> InterruptibleStream<Self>
     where
         Self: Sized,
     {
-        let (stream_trigger, stream_valved) = Valved::new(self);
-        tokio::task::spawn(async move {
-            let (Ok(InterruptSignal) | Err(_)) = interrupt_rx.await;
-
-            drop(stream_trigger);
-        });
-
-        InterruptibleStream::new(stream_valved)
+        InterruptibleStream::new(self, interrupt_rx.into())
     }
 
     #[cfg(feature = "ctrl_c")]
-    fn interruptible_ctrl_c(self) -> InterruptibleStream<Self>
+    fn interruptible_ctrl_c(self) -> InterruptibleStream<'static, Self>
     where
         Self: Sized,
     {
-        let (stream_trigger, stream_valved) = Valved::new(self);
+        let (interrupt_tx, interrupt_rx) = mpsc::channel::<InterruptSignal>(16);
         tokio::task::spawn(async move {
             tokio::signal::ctrl_c()
                 .await
                 .expect("Failed to initialize signal handler for `SIGINT`.");
 
-            drop(stream_trigger);
+            let (Ok(()) | Err(SendError(InterruptSignal))) =
+                interrupt_tx.send(InterruptSignal).await;
         });
 
-        InterruptibleStream::new(stream_valved)
+        InterruptibleStream::new(self, interrupt_rx.into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ops::ControlFlow;
+
+    use futures::{stream, StreamExt};
+    use tokio::sync::{
+        mpsc::{self, error::SendError},
+        oneshot,
+    };
+
+    use super::InterruptibleStreamExt;
+    use crate::InterruptSignal;
+
+    #[tokio::test]
+    async fn interrupt_overrides_stream_return_value() {
+        let (interrupt_tx, mut interrupt_rx) = mpsc::channel::<InterruptSignal>(16);
+        let (ready_tx, ready_rx) = oneshot::channel::<()>();
+
+        let mut interruptible_stream =
+            stream::unfold((0u32, Some(ready_rx)), move |(n, ready_rx)| async move {
+                if let Some(ready_rx) = ready_rx {
+                    let () = ready_rx.await.expect("Expected to be notified to start.");
+                }
+                if n < 3 {
+                    Some((n, (n + 1, None)))
+                } else {
+                    None
+                }
+            })
+            .interruptible(&mut interrupt_rx);
+
+        interrupt_tx
+            .send(InterruptSignal)
+            .await
+            .expect("Expected to send `InterruptSignal`.");
+        ready_tx
+            .send(())
+            .expect("Expected to notify sleep to start.");
+
+        let control_flow = interruptible_stream.next().await;
+
+        assert_eq!(
+            Some(ControlFlow::Break((InterruptSignal, 0u32))),
+            control_flow
+        );
+    }
+
+    #[tokio::test]
+    async fn interrupt_after_stream_completes_does_not_override_value() {
+        let (interrupt_tx, mut interrupt_rx) = mpsc::channel::<InterruptSignal>(16);
+
+        let mut interruptible_stream = stream::unfold(0u32, move |n| async move {
+            if n < 3 { Some((n, n + 1)) } else { None }
+        })
+        .interruptible(&mut interrupt_rx);
+
+        let control_flow = interruptible_stream.next().await;
+
+        let (Ok(()) | Err(SendError(InterruptSignal))) = interrupt_tx.send(InterruptSignal).await;
+
+        assert_eq!(Some(ControlFlow::Continue(0)), control_flow);
     }
 }
